1f699877429a2af0cef086db98d159d035286588
Derek Glassick <derek.glassick@gmail.com>
2020-02-29 16:24:13 -0800

    deleted math random functions and instead went for creating a master array that will push all arrays when selected into that array. Created a function that will pull from a multidimensional array and create the password.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,72 @@
+var yourpasswordEl = document.getElementById('yourpassword');
+var confirmLowercaseEl = document.getElementById('lowercase');
+var confirmUppercaseEl = document.getElementById('uppercase');
+var confirmNumbersEl = document.getElementById('numbers');
+var confirmSpecialEl = document.getElementById('specialCharacters');
+var lengthEl = document.getElementById('passwordLen');
+var generateButtonEl = document.getElementById('generateButton');
+var copyButton = document.getElementById('copyButton');
+// var passwordLength;
+// var passwordLengthValue;
+// var passwordLengthFinal;
 
-function getLowerCase () {    
-   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+   "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+   "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharacters = ["?", "!", "@", "$", "&", "*", "#"];
+var passwordArray = [];
 
-function getUpperCase () {
+
+
+// This is the event to trigger the password includes the options to confirm different characters
+generateButtonEl.addEventListener("click", function () {
+   
+   var passwordLength = lengthEl.value;
+   console.log(passwordLength);
+   var trueLowercase = confirmLowercaseEl.checked;
+   var trueUppercase = confirmUppercaseEl.checked;
+   var trueNumbers = confirmNumbersEl.checked;
+   var trueSpecial = confirmSpecialEl.checked;
+ generatePassword(trueLowercase, trueUppercase, trueNumbers, trueSpecial, passwordLength)
+});
+
+function generatePassword(lowerLetters, upperletters, numbersRan, symbols, passwordLength) {
+   var pass = "";
+   if(lowerLetters === true) {
+      passwordArray.push(lowercase)
+      console.log(passwordArray)
+   }
+   if(upperletters === true) {
+      passwordArray.push(uppercase)
+      console.log(passwordArray)
+   }
+   if(numbersRan === true) {
+      passwordArray.push(numbers)
+      console.log(passwordArray)
+   }
+   if(symbols === true) {
+      passwordArray.push(specialCharacters)
+      console.log(passwordArray)
+   }
+   console.log(passwordLength);
+   passwordMixer(passwordLength);
+};
+
+// function to generate the password
+
+function passwordMixer(value) {
+   console.log(value);
+   var newPassword = "";
+   for(var i = 0; i < value; i++){
+   var randomNumber = Math.floor(Math.random() * passwordArray.length);
+   var randomNumber2 = Math.floor(Math.random() * passwordArray[randomNumber].length);
+   // passwordArray[randomNumber][randomNumber2]
+   newPassword += passwordArray[randomNumber][randomNumber2];
+   }
+   newPassword.text = yourpasswordEl
+   console.log(newPassword);
+};
